6a607ee0ab68a1ee1daf6e64c04b56070e2b2524
MishaDakhnenko <114470203+MishaDakhnenko@users.noreply.github.com>
2022-12-11 18:30:17 +0300

    Update lab2-1.c

--- lab2-1.c
+++ lab2-1.c
@@ -7,7 +7,7 @@ int main()
 	scanf("%lf",&x);
 	if (x>1.5) y=(2.5)*(pow(x,3))+(6)*(pow(x,2))-30;
 	if ((x>=0) && (x<=1.5)) y=x+1;
-    if(x<0) y=x;
+        if(x<0) y=x;
 	printf("y: %0.6lf\n", y);
 	system("pause");
 	return 0;
