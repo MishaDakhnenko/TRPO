419ca293d8c51cdd602e30ba2f055bc6daec8726
MishaDakhnenko <114470203+MishaDakhnenko@users.noreply.github.com>
2022-12-11 18:29:44 +0300

    Add files via upload

---
+++ lab2-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	double x,y;
+	printf("Enter x: ");
+	scanf("%lf",&x);
+	if (x>1.5) y=(2.5)*(pow(x,3))+(6)*(pow(x,2))-30;
+	if ((x>=0) && (x<=1.5)) y=x+1;
+    if(x<0) y=x;
+	printf("y: %0.6lf\n", y);
+	system("pause");
+	return 0;
+} 
